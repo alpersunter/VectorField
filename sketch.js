c135151ba99e50c82e6782a01d4b2e8d0ec2d942
Alper Sünter <41203420+alpersunter@users.noreply.github.com>
2019-04-21 13:32:41 +0300

    Migration from keyPressed() to keyIsDown()

--- sketch.js
+++ sketch.js
@@ -24,6 +24,9 @@ function draw() {
   vf.show();
   pop();
   noFill();
+
+  keyboardControl(ball);
+
   ball.update();
   ball.show();
 }
@@ -36,22 +39,42 @@ function E(P) {
   return createVector(Vx, Vy, Vz);
 }
 
+function keyboardControl(b){
+  if(keyIsDown(UP_ARROW)) b.Up();
+  if(keyIsDown(DOWN_ARROW)) b.Down();
+  if(keyIsDown(LEFT_ARROW)) b.Left();
+  if(keyIsDown(RIGHT_ARROW)) b.Right(); 
+  // 70 and 16 are keyCodes for f and Shift
+  if(keyIsDown(70)&&!keyIsDown(16)) b.free = true;
+  if(keyIsDown(16)&&keyIsDown(70)) b.free = false;
+  // 107 and 187 are keyCodes for +
+  if(keyIsDown(107) || keyIsDown(187)) b.Forward();
+  // 109 and 189 are keyCodes for -
+  if(keyIsDown(109) || keyIsDown(189)) b.Backward();
+  // 82 r
+  if(keyIsDown(82)) b.Reset();
+  // 65 a
+  if(keyIsDown(65)) b.timeScale *= 0.5;
+  // 81 q
+  if(keyIsDown(81)) b.timeScale *= 2.0;
+}
+
 function keyPressed(){
-  if(key === 'f'){
-    ball.free = true;
-  }
-  if(key === 'F'){
-    ball.free = false;
-  }
+  // if(key === 'f'){
+  //   ball.free = true;
+  // }
+  // if(key === 'F'){
+  //   ball.free = false;
+  // }
 
   
-  if(keyCode === UP_ARROW) ball.Up();
-  if(keyCode === DOWN_ARROW) ball.Down();
-  if(keyCode === LEFT_ARROW) ball.Left();
-  if(keyCode === RIGHT_ARROW) ball.Right();
-  if(key === '+') ball.Forward();
-  if(key === '-') ball.Backward();
-  if(key === 'r') ball.Reset();
-  if(key ==='a') ball.timeScale *= 0.5;
-  if(key ==='q') ball.timeScale *= 2.0;
+  // if(keyCode === UP_ARROW) ball.Up();
+  // if(keyCode === DOWN_ARROW) ball.Down();
+  // if(keyCode === LEFT_ARROW) ball.Left();
+  // if(keyCode === RIGHT_ARROW) ball.Right(); 
+  // if(key === '+') ball.Forward();
+  // if(key === '-') ball.Backward();
+  // if(key === 'r') ball.Reset();
+  // if(key ==='a') ball.timeScale *= 0.5;
+  // if(key ==='q') ball.timeScale *= 2.0;
 }
